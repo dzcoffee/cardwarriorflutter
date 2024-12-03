fdc2fcb6e510ca9fae141de11e37b396aacc8451
cauyoon <williamha13@gmail.com>
2024-12-03 10:35:41 +0900

    Update customizing_page.dart

--- lib/ui/customizing_page.dart
+++ lib/ui/customizing_page.dart
@@ -114,7 +114,7 @@ class _CustomizingPageState extends State<CustomizingPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Deck Customizing'),
+        title: Text('Deck Customizing (${player!.cardDeck.length}/20)'),
         actions: [
           IconButton(
             icon: Icon(Icons.check),
@@ -127,135 +127,174 @@ class _CustomizingPageState extends State<CustomizingPage> {
         ],
       ),
       body: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3,
-          childAspectRatio: 258/378,
-          crossAxisSpacing: 8,
-          mainAxisSpacing: 8,
-        ),
-        itemCount: cardList.length,
-        itemBuilder: (context, index){
-          final card = cardList[index];
-          final isSelected = player!.cardDeck.contains(card);
-          int idx = card.id;
-          return GestureDetector(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            childAspectRatio: 258/378,
+            crossAxisSpacing: 8,
+            mainAxisSpacing: 8,
+          ),
+          itemCount: cardList.length,
+          itemBuilder: (context, index){
+            final card = cardList[index];
+            final isSelected = player!.cardDeck.contains(card);
+            int idx = card.id;
+            return GestureDetector(
               onTap: () => toggleCardSelection(card), // 카드 선택/해제 토글
-            child: Stack(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/images/cards/$idx.JPG'),
-                      fit: BoxFit.cover,
+              child: Stack(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/cards/$idx.JPG'),
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(
+                        color: isSelected ? Colors.blue : Colors.transparent,
+                        width: 3, // 선택 상태 강조
+                      ),
                     ),
-                    borderRadius: BorderRadius.circular(12),
-                    border: Border.all(
-                      color: isSelected ? Colors.blue : Colors.transparent,
-                      width: 3, // 선택 상태 강조
-                    ),
-                  ),
-                  child: Stack(
-                    children: [
-                      Align(
-                        alignment: Alignment.topLeft,
-                        child: Container(
-                          padding: EdgeInsets.symmetric(horizontal: 2, vertical: 4),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(6),
-                          ),
-                          child: SizedBox(
-                            width: 20,
-                            child: Text(
-                              '${card.cost}',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                fontSize: 17,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.blue,
+                    child: Stack(
+                      children: [
+                        Align(
+                          alignment: Alignment.topLeft,
+                          child: Container(
+                            padding: EdgeInsets.symmetric(horizontal: 2, vertical: 4),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(6),
+                            ),
+                            child: SizedBox(
+                              width: 20,
+                              child: Text(
+                                '${card.cost}',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.blue,
+                                ),
                               ),
                             ),
                           ),
                         ),
-                      ),
-                      Align(
-                        alignment: Alignment.bottomCenter,
-                        child: Container(
-                          margin: EdgeInsets.only(bottom: 7),
-                          padding: EdgeInsets.symmetric(horizontal: 3, vertical: 1),
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          child: Text(
-                            '${card.name}',
-                            style: TextStyle(
-                              fontSize: 10,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
+                        Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Container(
+                            margin: EdgeInsets.only(bottom: 7),
+                            padding: EdgeInsets.symmetric(horizontal: 3, vertical: 1),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(8),
                             ),
-                          ),
-                        ),
-                      ),
-                      Align(
-                        alignment: Alignment.bottomLeft,
-                        child: Container(
-                          padding: EdgeInsets.symmetric(horizontal: 2, vertical: 8),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(6),
-                          ),
-                          child: SizedBox(
-                            width: 20,
                             child: Text(
-                              '${card.atk}',
-                              textAlign: TextAlign.center,
+                              '${card.name}',
                               style: TextStyle(
-                                fontSize: 17,
+                                fontSize: 10,
                                 fontWeight: FontWeight.bold,
-                                color: Colors.red,
+                                color: Colors.black,
                               ),
                             ),
                           ),
                         ),
-                      ),
-                      Align(
-                        alignment: Alignment.bottomRight,
-                        child: Container(
-                          padding: EdgeInsets.symmetric(horizontal: 1, vertical: 8),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(6),
-                          ),
-                          child: SizedBox(
-                            width: 20,
-                            child: Text(
-                              '${card.hp}',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                fontSize: 17,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.green,
+                        Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Container(
+                            padding: EdgeInsets.symmetric(horizontal: 2, vertical: 8),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(6),
+                            ),
+                            child: SizedBox(
+                              width: 20,
+                              child: Text(
+                                '${card.atk}',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.red,
+                                ),
                               ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-                // 선택 표시 (체크 아이콘)
-                if (isSelected)
-                  Align(
-                    alignment: Alignment.topRight,
-                    child: Icon(
-                      Icons.check_circle,
-                      color: Colors.blue,
-                      size: 30,
+                        Align(
+                          alignment: Alignment.bottomRight,
+                          child: Container(
+                            padding: EdgeInsets.symmetric(horizontal: 1, vertical: 8),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(6),
+                            ),
+                            child: SizedBox(
+                              width: 20,
+                              child: Text(
+                                '${card.hp}',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.green,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-              ],
-            ),
-          );
-        }
-      )
+                  // 선택 표시 (체크 아이콘)
+                  if (isSelected)
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: Icon(
+                        Icons.check_circle,
+                        color: Colors.blue,
+                        size: 30,
+                      ),
+                    ),
+                ],
+              ),
+            );
+          }
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          print('Selected Cards: ${player!.cardDeck}');
+          if(player!.cardDeck.length < 20) {
+            showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: const Text('잠깐!'),
+                  content: const Text('20장을 선택해주세요!'),
+                  actions: [
+                    TextButton(
+                      child: const Text('확인'),
+                      onPressed: () => Navigator.pop(context),
+                    )
+                  ],
+                )
+            );
+          } else if(player!.cardDeck.length > 20){
+            showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: const Text('잠깐!'),
+                  content: const Text('20장만 가져갈 수 있습니다!'),
+                  actions: [
+                    TextButton(
+                      child: const Text('확인'),
+                      onPressed: () => Navigator.pop(context),
+                    )
+                  ],
+                )
+            );
+          } else{
+            saveWarriorsToDB(_username!, player!.cardDeck);
+            _showSaveDeckPopup();
+          }
+        },
+        child: Icon(Icons.add, size: 30,),
+      ),
     );
   }
 }
+
